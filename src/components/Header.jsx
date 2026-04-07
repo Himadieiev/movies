4b2653799020dd4c152228aa9cb909d31a314e65
Himadieiev Ruslan <gimadejev86@gmail.com>
2026-04-07 12:21:11 +0300

    Add logic to hide the Hero Banner in the Header component on the MovieDetailsPage.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,22 @@
+import {useLocation} from "react-router-dom";
+
 import Navigation from "./Navigation";
 
 const Header = () => {
+  const location = useLocation();
+  const isDetailsPage = location.pathname.startsWith("/movie/");
+
   return (
     <header>
       <Navigation />
-      <img src="/hero.png" alt="Hero Banner" width={512} height={476} loading="lazy" />
+      <img
+        src="/hero.png"
+        alt="Hero Banner"
+        width={512}
+        height={476}
+        loading="lazy"
+        className={isDetailsPage ? "hidden" : ""}
+      />
     </header>
   );
 };
