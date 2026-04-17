4367691fc034fdb8a757af1949b39b9977e5662a
Himadieiev Ruslan <gimadejev86@gmail.com>
2026-04-17 11:46:19 +0300

    Update Navigation with TrendingPage link and styles.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -102,6 +102,27 @@ const Navigation = () => {
         </svg>
         <span>Unwatched</span>
       </NavLink>
+      <NavLink
+        to="/trending"
+        data-tooltip="Trending"
+        className={({isActive}) => `nav-link ${isActive ? "active" : ""}`}
+      >
+        <svg
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="trending-icon"
+        >
+          <path d="M16.5 9.5L12.3 13.7L10.7 11.3L7.5 14.5" />
+          <path d="M14.5 9.5H16.5V11.5" />
+          <path d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z" />
+        </svg>
+
+        <span>Trending</span>
+      </NavLink>
     </nav>
   );
 };
